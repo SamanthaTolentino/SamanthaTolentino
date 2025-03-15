9fe3bfa94d2b8ff6895463cfe683867a5d7db148
Samantha Tolentino <s.tolentino0828@gmail.com>
2025-03-15 14:45:36 -0400

    Added nav and styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,32 @@ import SectionAbout from './sections/SectionAbout'
 import HamburgerMenu from './assets/svg/HamburgerMenu'
 
 function App() {
+  const getNavList = () => {
+    return <div className='text-5xl'>
+      {
+        [...Array(4)].map((i) => {
+          return <div key={i} className='mb-5'>•</div>
+        })
+      }
+    </div>
+  }
 
   return (
-    <div className='karla-font'>
-      <SectionAbout />
-      {/* <div className='h-96 bg-blue-1'>
-        
-      </div> */}
+    <div className='karla-font pt-15'>
+      <div className='grid grid-cols-12'>
+        <div className='col-start-3 col-span-8 pb-32'>
+          <SectionAbout />
+        </div>
+        <div className='col-start-11 flex flex-col items-center justify-between'>
+          <button>
+            <HamburgerMenu />
+          </button>
+          {getNavList()}
+          <div></div>
+        </div>
+
+        <div className='row-start-2 bg-yellow-1 col-span-12 h-96'>hi</div>
+      </div>
     </div>
   )
 }
--- src/sections/SectionAbout.jsx
+++ src/sections/SectionAbout.jsx
@@ -5,18 +5,20 @@ import profilePic from '../assets/img/profile_pic.png'
 
 const SectionAbout = () => {
   return (
-    <div className='flex max-w-4xl mx-auto py-32 text-brown-1'>
+    <div className='flex max-w-4xl mx-auto text-brown-1'>
         <div className='flex flex-col items-center'>
-            <div className='flex items-center mb-14 mt-20'>
-                <div className='relative mr-20'>
-                    <div className='relative bg-yellow-1 aspect-square w-full max-w-2xs min-w-2xs rounded-full overflow-clip flex items-end justify-center'>
-                        <img src={profilePic} alt="profile_pic.png" className='w-2/3 absolute top-1/9'  />
-                    </div>
-                    <div className='absolute top-0 -left-10 '>
-                        <Stars />
-                    </div>
-                    <div className='absolute top-10 -left-12 rotate-5'>
-                        <Flower />
+            <div className='flex items-center mb-12 mt-20'>
+                <div className='mr-20'>
+                    <div className='relative'>
+                        <div className='relative bg-yellow-1 aspect-square w-full max-w-2xs min-w-2xs rounded-full overflow-clip flex items-end justify-center'>
+                            <img src={profilePic} alt="profile_pic.png" className='w-2/3 absolute top-1/9'  />
+                        </div>
+                        <div className='absolute top-2 -left-2 -rotate-8'>
+                            <Flower petals='#91BEF8' pistil='#FFF9E6' width={30} />
+                        </div>
+                        <div className='absolute top-9 -left-10 rotate-5'>
+                            <Flower petals='#FA6255' pistil='#FFF9E6' width={40} />
+                        </div>
                     </div>
                 </div>
                 <div className='grow flex flex-col font-bold'>      
@@ -26,7 +28,9 @@ const SectionAbout = () => {
                     <p className='font-medium mb-4'>S.Tolentino0828@gmail.com</p>
                     <p className='text-pink-1 uppercase mb-1'>LinkedIn</p>
                     {/* Make link */}
-                    <p className='font-medium'>www.linkedin.com/in/samanthatolentino/</p>
+                    <p className='font-medium mb-4'>www.linkedin.com/in/samanthatolentino/</p>
+                    <p className='text-pink-1 uppercase mb-1'>GitHub</p>
+                    <p className='font-medium mb-4'>www.github.com/SamTole</p>
                 </div>
             </div>
             <div className='px-10'>
